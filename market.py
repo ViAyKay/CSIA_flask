5f7b4a3d01c1fa1f9a637758ab53d000e570c7f3
varunkarthikeyan101@gmail.com <koolfirek@gmail.com>
2022-10-04 15:32:33 +0530

    Finished some fixes and create first table

--- market.py
+++ market.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from pydoc import synopsis
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://computer:spark@localhost/library_sys'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    synopsis = db.Column(db.Text, nullable=True)
+    cover_image = db.Column(db.BLOB, nullable=False)
+    available = db.Column(db.Boolean, nullable=False, default=True)
 
 
 @app.route('/')
