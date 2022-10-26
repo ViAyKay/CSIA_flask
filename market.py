db05c5354c7cd4fb78b2cc17d92616c51796d997
varunkarthikeyan101@gmail.com <koolfirek@gmail.com>
2022-10-26 20:51:56 +0530

    First Commit finished login form

--- market.py
+++ market.py
@@ -3,6 +3,9 @@ from pydoc import synopsis
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from sqlalchemy import ForeignKey
 
@@ -10,6 +13,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://computer:spark@localhost/library_sys'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = "whatgoesupmustneverstayupforthedevillooksforanomalousbasterds101"
+
+#Create a form class
+class LoginForm(FlaskForm):
+    name = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log In")
 
 class Book (db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,23 +28,36 @@ class Book (db.Model):
     cover_image = db.Column(db.BLOB, nullable=False)
     available = db.Column(db.Boolean, nullable=False, default=True)
 
-class Users (db.Model):
+class Librarian (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.string(100), nullable=False, unique=True)
-    email = db.Column(db.string(200), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(200), nullable=False, unique=True)
     superuser = db.Column(db.Boolean, nullable=False)
 
 class Borrow (db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, default=date.utcnow)
+    #date = db.Column(db.Date, default=date.utcnow)
     book_id = db.Column(db.Integer, ForeignKey(Book.id))
-    user_id = db.Column(db.Integer, ForeignKey(Book.id))
     return_date = db.Column(db.Date, nullable=False) 
 
 
 
-@app.route('/')
-def home_page():
-    return render_template('loginpage.html') 
+@app.route('/', methods=['GET', 'POST'])
+def login_page():
+    name = None
+    password = None
+    form = LoginForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        password = form.password.data
+        form.name.data = ''
+        form.password.data =''
+
+    return render_template('loginpage.html', name = name, password = password, form = form) 
+
+
+
+
 
